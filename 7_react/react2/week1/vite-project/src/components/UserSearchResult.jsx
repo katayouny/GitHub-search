9c7609bd0537b5cb8e9848ae043005a736043660
katayouny <k.yousefzadeh@gmail.com>
2024-04-12 22:25:35 +0200

    was missing in first upload

---
+++ 7_react/react2/week1/vite-project/src/components/UserSearchResult.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { userContext } from "./UserSection";
+
+function UserSearchResult() {
+  const { query, loading, error, searchResult } = useContext(userContext);
+
+  return (
+    <div>
+      <h4>User Search Result</h4>
+
+      {loading && <div style={{color: "darkblue"}}>Loading...</div>}
+      {error && <div style={{color: "brown"}}>Error fetching data: {error}</div>}
+      {!loading && searchResult.length === 0 && <div style={{color: "brown"}}>No match found</div>}
+      {!loading && searchResult.length > 0 && (<ul>
+          {searchResult.map((result) => (
+            <li key={resultIndex}> {result.toLowerCase().includes({query}.toLowerCase())}</li>))}
+        </ul>)}
+      <br />
+    </div>
+  );
+}
+
+export default UserSearchResult;
